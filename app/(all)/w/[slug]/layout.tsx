0030a51876e5a4ffbc9d920cbed8d6e78a9c505c
tepythai <thaitepy@gmail.com>
2023-05-27 14:41:28 +0900

    chore

--- app/(all)/w/[slug]/layout.tsx
+++ app/(all)/w/[slug]/layout.tsx
@@ -7,7 +7,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         'mx-auto',
         'py-12 prose prose-lg prose-teal prose-h1:font-canela prose-h1:text-3xl',
         'md:prose-xl xl:prose-2xl md:prose-h1:text-4xl lg:prose-h1:text-5xl',
-        'prose-pre:max-w-[90vw] code-block prose-code:before:content-none prose-code:after:content-none'
+        'prose-pre:max-w-[90vw] prose-pre:rounded-none',
+        'prose-code:before:content-none prose-code:after:content-none'
       )}
     >
       {children}
